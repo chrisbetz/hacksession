32d02c6e0878d62fc4d0949e1527a3aaccdf7fff
Steffen Frank <steffen.frank@mgm-tp.com>
2015-04-23 18:32:29 +0200

    moved handlers and subscriptions to separate namespaces

---
+++ src/cljs/phonecat_re_frame/subs.cljs
@@ -0,0 +1,60 @@
+(ns phonecat-re-frame.subs
+  (:require [re-frame.core :as re-frame])
+  (:require-macros [reagent.ratom :refer [reaction]]))
+
+;; -------------------------
+;; Re-frame subscriptions
+
+(re-frame/register-sub                                      ;; a new subscription handler
+  :phones                                                   ;; usage (subscribe [:phones])
+  (fn [db]
+      ;; extracts the phones property from the db
+      (reaction (:phones @db))))                            ;; pulls out :phones
+
+(re-frame/register-sub
+  :search-input
+  (fn [db]
+      (reaction (:search-input @db))))
+
+(re-frame/register-sub                                      ;; a new subscription handler
+  :phones                                                   ;; usage (subscribe [:phones])
+  (fn [db]
+      (reaction (:phones @db))))                            ;; pulls out :phones
+
+(re-frame/register-sub
+  :selected-image-url
+  (fn
+    ;; extract the selected-image-url from the db. If it's not set return the first image of the current phone under query
+    [db [_ phone-id]]
+    (let [phone (re-frame/subscribe [:phone-query phone-id])
+          phone-details (re-frame/subscribe [:phone-details])
+          images (reaction (:images @phone))]
+         ;; Note how we are sequencing reactions above. Whenever the phone ratom changes the images ratom will change as well
+         (reaction
+           (if @phone-details
+             (if-let [image-url (:selected-image-url @phone-details)]
+                     image-url
+                     (first @images)))))))
+
+(re-frame/register-sub
+  :phone-details
+  (fn [db]
+      (reaction (:phone-details @db))))
+
+(re-frame/register-sub
+  :order-prop
+  (fn [db]
+      (reaction (:order-prop @db))))
+
+(re-frame/register-sub
+  :phone-details
+  (fn [db]
+      (reaction (:phone-details @db))))
+
+(re-frame/register-sub
+  :phone-query
+  (fn
+    ;; get info on the given phone id from the phone-details map
+    [db [_ phone-id]]
+    (let [phone-details-reaction (reaction (:phone-details @db))]
+         (reaction ((keyword phone-id) @phone-details-reaction)))))

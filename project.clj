d1a025304a2d14e45bca295dfe68e31fc1cf7ee6
Steffen Frank <steffen.frank@mgm-tp.com>
2015-05-04 12:45:42 +0200

    upgraded re-frame version to 0.4.0

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                            [cljsjs/react "0.12.2-5"]
                            [reagent "0.5.0"]
                            [reagent-utils "0.1.2"]
-                           [secretary "1.2.1"]
+                           [secretary "1.2.3"]
                            [org.clojure/clojurescript "0.0-3211" :scope "provided"]
                            [ring "1.3.2"]
                            [ring/ring-defaults "0.1.3"]
@@ -18,7 +18,7 @@
                            [compojure "1.3.2"]
                            [selmer "0.8.0"]
                            [environ "1.0.0"]
-                           [re-frame "0.2.0"]
+                           [re-frame "0.4.0"]
                            [cljs-ajax "0.3.10"]]
 
             :plugins [
